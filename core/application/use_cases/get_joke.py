7c909e3cee9bd5f7dfccd2d3e8eaa8111734dbf9
Kassio Wallace <kassiowl34@gmail.com>
2023-05-06 16:58:04 -0300

    Get joke fix

--- core/application/use_cases/get_joke.py
+++ core/application/use_cases/get_joke.py
@@ -2,7 +2,7 @@
 
 from core.domain.repositories.get_joke import GetJokeDefinition
 
-class GetAveragePrice():
+class GetJoke():
     def __init__(self, get_joke_impl: GetJokeDefinition):
         self.get_joke_impl = get_joke_impl
     
